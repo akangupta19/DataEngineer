c6b79d684445500169048fd41092d7d782298bde
akangupta19 <akangupta1991@gmail.com>
2019-06-20 17:41:44 +0530

    program

---
+++ Tuple/program10.py
@@ -0,0 +1,10 @@
+x = ("w3resource")
+
+y = reversed(x)
+print(tuple(y))
+
+x = (5, 10, 15, 20)
+
+y = reversed(x)
+print(tuple(y))
+
---
+++ Tuple/program9.py
@@ -0,0 +1,29 @@
+tuplex = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+
+sd = tuplex[3:5]
+
+print(sd)
+
+sd = tuplex[:6]
+print(sd)
+
+sd = tuplex[5:]
+print(sd)
+
+sd = tuplex[:]
+print(sd)
+
+sd = tuplex[-8:-4]
+print(sd)
+
+tuplex = tuple("HELLO WORLD")
+print(tuplex)
+
+sd = tuplex[2:9:2]
+print(sd)
+
+sd = tuplex[::4]
+print(sd)
+
+
+
