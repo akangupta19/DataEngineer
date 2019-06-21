416a433d4f201a06daa88c1f69ce9d19b432e676
akangupta19 <akangupta1991@gmail.com>
2019-06-21 17:29:25 +0530

    program

---
+++ stringprogram/program1.py
@@ -0,0 +1,5 @@
+str=raw_input("Enter string:")
+count=0
+for i in str:
+      count=count+1
+print('\nLength of the string is:', count)
---
+++ stringprogram/program7.py
@@ -0,0 +1,7 @@
+str1 = raw_input("Enter String:")
+words=str1.split(",")
+words.sort().dictinct()
+print(words)
+#for word in words:
+#    print(word)
+
