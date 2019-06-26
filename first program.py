fc23511336b2cda2544a6d2f9524e68a3e6b45fa
akangupta19 <akangupta1991@gmail.com>
2019-06-26 12:34:57 +0530

    program

--- first program.py
+++ first program.py
@@ -1,7 +1,24 @@
-word=raw_input("Enter Full Name with space")
+'''word=raw_input("Enter Full Name with space")
 
 for char in range(len(word) - 1, -1, -1):
 
 
   print(word[char])
 print("\n")
+'''
+
+def reverse(s):
+  str = ""
+  for i in s:
+   # print(i)
+    str = i+" "+ str
+    #print(str)
+  return str
+
+print("Hello World")
+fval = raw_input("Enter your fname: ")
+lval = raw_input("Enter your lname: ")
+
+print("----------------------------------------")
+print("reverse " + reverse(fval+lval))
+
